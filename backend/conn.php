e8911236bd2b7b2a6cd9e47a8b019b89dd0182df
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2022-04-14 12:23:30 +0200

    Setup

--- backend/conn.php
+++ backend/conn.php
@@ -1,10 +1,16 @@
 <?php
-   // connect to mongodb
-   $m = new MongoClient();
-	
-   echo "Connection to database successfully";
-   // select a database
-   $db = $m->mydb;
-	
-   echo "Database mydb selected";
+   require 'vendor/autoload.php';
+   use MongoDB\Client as Mongo;
+
+   $user = "admin";
+   $pwd = 'password';
+
+   $mongo = new Mongo("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false");
+   $db = $mongo->solyx;
+   $db_users = $db->users;
+   $query = $db_users->find(['name' => "TheMaksoo"])->toArray();
+
+   print_r($query);
+   
+ 
 ?>
--- index.php
+++ index.php
@@ -10,17 +10,15 @@
     <?php require_once 'header.php'; ?>
     <div class="container home">
         <h1>Solyx main page.</h1>
-    
-    <?php 
-    
-    require_once '../backend/conn.php';
-    
-    $query = "SELECT * FROM meldingen";
-    $statement = $conn->prepare($query);
-    $statement->execute([":id" => $id]);
-    $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
-    ?>
-    <textarea name='overige_info' id='overige_info' class='form-input' rows='4'><?php echo "{$meldingen}"?></textarea>
+        
+        <?php 
+        
+        require_once 'backend/conn.php';
+        $query = $db->find(['name' => "TheMaksoo"])->toArray();
+
+        
+        ?>
+        <textarea name='overige_info' id='overige_info' class='form-input' rows='4'><?php echo "{$query}"?></textarea>
     </div>
 
 </body>
