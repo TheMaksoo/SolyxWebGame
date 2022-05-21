f6cf999e9d77760ad868788f4ddd76fdac6060fb
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2022-05-21 18:26:58 +0200

    Update heal.php

--- content/fight/skills/heal.php
+++ content/fight/skills/heal.php
@@ -1,3 +1,17 @@
 <?php 
-
+if ($GLOBALS["userinfo"]["health"] == $GLOBALS["userinfo"]["MaxHealth"]){
+    $list .= "You already have ". $GLOBALS["userinfo"]["MaxHealth"] . " HP!";
+}
+if ($GLOBALS["userinfo"]["hp_potions"] > 0){
+    $gain = rand(25, 55);
+    $GLOBALS["userinfo"]["health"] += $gain;
+    if ($GLOBALS["userinfo"]["health"] > $GLOBALS["userinfo"]["MaxHealth"]){
+        $GLOBALS["userinfo"]["health"] = $GLOBALS["userinfo"]["MaxHealth"];
+    }
+    $GLOBALS["userinfo"]["hp_potions"] -= 1;
+    $list .= "You used a Health Potion <br> +".$gain ." HP";
+}
+else{
+$list .= " You don't have any health potions!";
+}
 ?>
